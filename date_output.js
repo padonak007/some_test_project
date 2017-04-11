42ff5b2da5a04c858987520df69e502f4963c481
illia <itchernikow@gmail.com>
2017-04-11 19:20:03 +0300

    Initial commit

---
+++ date_output.js
@@ -0,0 +1,41 @@
+/**
+ * Created by illiach on 11.04.2017.
+ */
+var currentDate = new Date();
+var name = 'Illia';
+console.log("Current month is:" + currentDate.getMonth() + "\nCurrent day of week is:" + currentDate.getDay());
+
+function getNameofDay(date) {
+    var dayNames = [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday',
+    ]
+    var currentDayofWeek = date.getDay();
+    return dayNames[currentDayofWeek];
+}
+
+function getNameofMonth(date){
+    var monthNames = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December',
+    ]
+    var currentMonth = date.getMonth();
+    return monthNames[currentMonth];
+}
+
+console.log("Today is: "+getNameofDay(currentDate)+"\nCurrent month is: "+getNameofMonth(currentDate)+"\nMy name is "+name);
---
+++ zeros.js
@@ -0,0 +1,11 @@
+/**
+ * Created by illiach on 11.04.2017.
+ */
+var zero='0';
+
+for (var i=0; i<5;i++){
+    console.log(zero);
+    zero=zero+0;
+}
+
+//
