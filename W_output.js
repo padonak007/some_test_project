99a39a0ca2d6b17ca49b39a9d872bd7f2f80091e
illia <itchernikow@gmail.com>
2017-04-12 13:17:47 +0300

    Вывести на экран букву "W" из символов "*" (звездочка).    Источник: http://www.itmathrepetitor.ru/prog/zadachi-na-vychisleniya/

--- W_output.js
+++ W_output.js
@@ -1,3 +1,11 @@
 /**
  * Created by illiach on 11.04.2017.
  */
+var line1 ="*     *     *";
+var line2 =" *   * *   *";
+var line3 ="  * *   * *";
+var line4 ="   *     *";
+console.log(line1);
+console.log(line2);
+console.log(line3);
+console.log(line4);
